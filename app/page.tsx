21a290e731aa7fc84b003eb0b2acd89b9635a220
Habib Afolabi <habibwritescode@gmail.com>
2025-08-03 12:43:29 +0100

    feat: test ssr title

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,37 @@ import Hero from './ui/home/hero';
 import ShopCategory from './ui/shop-categories/shop-categories';
 import ProductsShowcase from './ui/home/products-showcase';
 
+import { headers } from 'next/headers';
+import type { Metadata } from 'next';
+// import { Metadata } from 'next';
+
+const META_TITLE_MAP: Record<string, string> = {
+  'audiophile-ochre.vercel.app': "Habib's Audiophile",
+  here: 'test failed2',
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+  const allHeaders = await headers();
+  const host = allHeaders.get('host');
+
+  const baseHostname = host?.replace(/^www\./, '') || 'here';
+  console.log('baseHostName', baseHostname);
+  const title = META_TITLE_MAP[baseHostname] || 'Test failed';
+
+  return {
+    title,
+    description: `${title} - Welcome to hell`,
+    // openGraph: {
+    //   title,
+    //   description: '',
+    //   url: `https://${host}`,
+    //   siteName: title,
+    //   images: [`https://${host}/og-image.png`],
+    //   type: 'website',
+    // },
+  };
+}
+
 export default function Home() {
   return (
     <div>
